961ca68c2c771778c09ea9f5e2d6c8ebdda8c4b1
Nimai Patel <nimai.m.patel@gmail.com>
2020-07-28 10:53:42 +0530

    Added comments

--- euler51/euler51.cpp
+++ euler51/euler51.cpp
@@ -5,6 +5,7 @@
 #include <ostream>
 #include <vector>
 
+// function to repalce a digit at a positon(0-indexed from right) in original_number with new_digit
 int replace_digit(int original_number, int position, int new_digit) {
     int new_number;
     int archive = original_number;
@@ -18,6 +19,7 @@ int replace_digit(int original_number, int position, int new_digit) {
     return new_number;
 }
 
+// calculates number of digits
 int num_of_digits(int number) {
     int number_of_digits = 0;
     while (number != 0) {
@@ -27,6 +29,7 @@ int num_of_digits(int number) {
     return number_of_digits;
 }
 
+// takes in integer and returns vector with values containing indices which are TRUE for binary representation of number
 std::vector<int> get_indices(int number) {
     bool binary[32];
     memset(binary, false, 32);
@@ -45,6 +48,9 @@ std::vector<int> get_indices(int number) {
     return indices;
 }
 
+// check for prime numbers by replacing digits at positions given by indices with digits 0-9
+// counter gets incremented when prime number is generated
+// returns smallest prime from generated list IF number of primes is 8, otherwise returns -1
 int check_specific_pattern(int prime, bool* bool_array, std::vector<int> indices) {
     int store;
     int counter = 0;
@@ -64,10 +70,11 @@ int check_specific_pattern(int prime, bool* bool_array, std::vector<int> indices
         }
         if (bool_array[new_number]) counter++;
     }
-
     return counter == 8 ? store : -1;
 }
 
+// invoke check_specific_pattern function using possible combinations of digits to replace
+// see get_indices function to see how digit combinations are created
 int check_pattern(int prime, bool* bool_array) {
     int number_of_digits = num_of_digits(prime);
     for (int i = 1; i < pow(2, number_of_digits) - 1; i++) {
@@ -82,6 +89,7 @@ int main() {
     bool bool_array[MAX];
     memset(bool_array, true, MAX);
 
+    // Prime number algorithm (Erasthoneses Seive)
     for (int i = 2; i * i < MAX; i++) {
         if (bool_array[i]) {
             for (int j = i * i; j < MAX; j += i) {
